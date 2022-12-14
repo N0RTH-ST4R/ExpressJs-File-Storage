146d1fcd454a61299e8960f0efd30b8c05f6ed92
Storm <Samjpoyner@icloud.com>
2022-12-14 17:41:31 +0000

    made basic server stuff

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+//const data=require('./data.json')
+const fs=require('fs')
+const express=require('express')
+const app=express()
 
+const port=8080
+
+var data=JSON.parse(fs.readFileSync('data.json'))
+
+app.get("/",(req,res)=>{
+    res.sendFile('data.json',{ root: __dirname })
+})
+
+function addFile(name){
+    //FIX
+    data.name={
+        "data":fs.readFile('./'+name, 'utf8',()=>{})
+    }
+}
+
+addFile("test")
+console.log(data)
+
+app.listen(port,()=>{
+    console.log("Server on "+port)
+})
